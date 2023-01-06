dc5e3e36acb196a7ee19d9019562fe2afd65c3f5
sanjei PRanav <sanjeipranav.me19@bitsathy.ac.in>
2023-01-06 16:18:47 +0530

    Added Ui elements for OwnerExpenseTemplate

---
+++ SPLITTR_Uwp/DataTemplates/OwnerExpenseUserControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SPLITTR_Uwp.DataTemplates
+{
+    public sealed partial class OwnerExpenseUserControl : UserControl
+    {
+        public OwnerExpenseUserControl()
+        {
+            this.InitializeComponent();
+            DataContextChanged += OwnerExpenseUserControl_DataContextChanged;
+        }
+
+        private void OwnerExpenseUserControl_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            Bindings.Update();
+        }
+    }
+}
