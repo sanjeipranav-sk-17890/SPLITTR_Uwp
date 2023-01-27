e4bc05c391e6619f374172bdd0719cd294b4b087
sanjei PRanav <sanjeipranav.me19@bitsathy.ac.in>
2023-01-27 12:58:20 +0530

    Revert "Revert "Application View initial Changed""
    This reverts commit f1695e9294cb047b00968a7c4557ced5a2b15c0c.

--- SPLITTR_Uwp/Views/AddExpenseTestPage.xaml.cs
+++ SPLITTR_Uwp/Views/AddExpenseTestPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.WindowManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -26,20 +29,20 @@ namespace SPLITTR_Uwp.Views
 
         private async void StateService_OnUserLoggedOut(Core.ModelBobj.UserBobj obj)
         {
-            if (_sptrAppWindow is null || !_sptrAppWindow.IsVisible)
-            {
-                return;
-            }
-            await _sptrAppWindow.CloseAsync();
+            //if (_sptrAppWindow is null || !_sptrAppWindow.IsVisible)
+            //{
+            //    return;
+            //}
+            //await _sptrAppWindow.CloseAsync();
         }
 
-        private AppWindow _sptrAppWindow = null;
+        private ApplicationView _sptrAppView = null;
 
-        private UserControl SplitExpenseUCtrl { get; set; }
+        private SplitExpenseUserControl SplitExpenseUCtrl { get; set; }
 
         private Grid _rootGrid = null;
 
-        private UserControl SeparateExpenseSplitControl()
+        private SplitExpenseUserControl SeparateExpenseSplitControl()
         {
             var splitExpenseUCtrl = MainGrid.FindName(nameof(SplitExpneseControl)) as SplitExpenseUserControl;
             MainGrid.Children.Remove(splitExpenseUCtrl);
@@ -51,53 +54,113 @@ namespace SPLITTR_Uwp.Views
         }
 
 
+        //private async void SplitExpenseControl_OnDragStarting(UIElement sender, DragStartingEventArgs args)
+        //{
+        //    //Removing UserControl Form Grid and Storing The Reference of it
+        //    SplitExpenseUCtrl = SeparateExpenseSplitControl();
+
+        //   if (_sptrAppView is null)
+        //   {
+        //       _sptrAppView = await AppWindow.TryCreateAsync();
+
+        //       //Assign separated ctrl to New Window root Element
+        //       _rootGrid = new Grid();
+        //       _rootGrid.Children.Add(SplitExpenseUCtrl);
+
+        //        //Registering Root Xaml Element To ThemeHelper onLoaded 
+        //        _rootGrid.Loaded += (o, eventArgs) =>
+        //        {
+        //            //Applying Current Theme To Root Grid
+        //            _rootGrid.RequestedTheme = ThemeHelperService.GetPreferenceThemeIfSet();
+        //            ThemeHelperService.RegisterElement(_rootGrid);
+        //            SplitExpenseUCtrl.XamlRoot = _rootGrid.XamlRoot;
+        //        };
+
+        //        SetMinimumWindowWidth(_sptrAppView,new Size(500,700));
+
+        //        //Setting Window Content Property to root Grid
+        //        ElementCompositionPreview.SetAppWindowContent(_sptrAppView, _rootGrid);
+        //       _sptrAppView.RequestMoveAdjacentToCurrentView();
+
+        //       _sptrAppView.Closed += SptrAppWindowClosed;
+
+        //   }
+        //   await _sptrAppView.TryShowAsync();
+
+        //}
         private async void SplitExpenseControl_OnDragStarting(UIElement sender, DragStartingEventArgs args)
         {
             //Removing UserControl Form Grid and Storing The Reference of it
             SplitExpenseUCtrl = SeparateExpenseSplitControl();
 
-           if (_sptrAppWindow is null)
-           {
-               _sptrAppWindow = await AppWindow.TryCreateAsync();
+            if (_sptrAppView is null)
+            {
+                var newView = CoreApplication.CreateNewView();
 
-               //Assign separated ctrl to New Window root Element
-               _rootGrid = new Grid();
-               _rootGrid.Children.Add(SplitExpenseUCtrl);
 
-                //Registering Root Xaml Element To ThemeHelper onLoaded 
-                _rootGrid.Loaded += (o, eventArgs) =>
+               await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, (() =>
                 {
-                    //Applying Current Theme To Root Grid
-                    _rootGrid.RequestedTheme = ThemeHelperService.GetPreferenceThemeIfSet();
-                    ThemeHelperService.RegisterElement(_rootGrid);
-                    SplitExpenseUCtrl.XamlRoot = _rootGrid.XamlRoot;
-                };
+                    //Assign separated ctrl to New Window root Element
+                    _rootGrid = new Grid();
+                    var splitCntrl = new SplitExpenseUserControl(SplitExpenseUCtrl);
+                    _rootGrid.Children.Add(splitCntrl);
 
-                SetMinimumWindowWidth(_sptrAppWindow,new Size(500,700));
+                    //Registering Root Xaml Element To ThemeHelper onLoaded 
+                    _rootGrid.Loaded += (o, eventArgs) =>
+                    {
+                        //Applying Current Theme To Root Grid
+                        _rootGrid.RequestedTheme = ThemeHelperService.GetPreferenceThemeIfSet();
+                        ThemeHelperService.RegisterElement(_rootGrid);
+                      //  SplitExpenseUCtrl.XamlRoot = _rootGrid.XamlRoot;
+                    };
 
-                //Setting Window Content Property to root Grid
-                ElementCompositionPreview.SetAppWindowContent(_sptrAppWindow, _rootGrid);
-               _sptrAppWindow.RequestMoveAdjacentToCurrentView();
+                    Window.Current.Content = _rootGrid;
 
-               _sptrAppWindow.Closed += SptrAppWindowClosed;
-                
-           }
-           await _sptrAppWindow.TryShowAsync();
+                    Window.Current.Activate();
+                   _sptrAppView = ApplicationView.GetForCurrentView();
+
+                   _sptrAppView.Consolidated += _sptrAppView_Consolidated;
+
+                }));
+
+               await ApplicationViewSwitcher.TryShowAsStandaloneAsync(_sptrAppView.Id);
+
+
+
+               // SetMinimumWindowWidth(_sptrAppView, new Size(500, 700));
+
+               //Setting Window Content Property to root Grid
+               //  ElementCompositionPreview.SetAppWindowContent(_sptrAppView, _rootGrid);
+               //  _sptrAppView.RequestMoveAdjacentToCurrentView();
+
+               //   _sptrAppView.Closed += SptrAppWindowClosed;
+
+            }
+             //  await _sptrAppView.TryShowAsync();
 
         }
+
+        private void _sptrAppView_Consolidated(ApplicationView sender, ApplicationViewConsolidatedEventArgs args)
+        {
+            _sptrAppView.Consolidated -= _sptrAppView_Consolidated;
+            ThemeHelperService.UnRegisterElement(_rootGrid);
+            _sptrAppView = null;
+            _rootGrid = null;
+        }
+
         private void SetMinimumWindowWidth(AppWindow window, Size minimumWindowWidth)
         {
             // If specified size is smaller than the default min size for a window we need to set a new preferred min size first.
             // Let's set it to the smallest allowed and leave it at that.
-            WindowManagementPreview.SetPreferredMinSize(_sptrAppWindow, new Size(800, 1000));
+           // WindowManagementPreview.SetPreferredMinSize(_sptrAppView, new Size(800, 1000));
 
             // Request the size of our window
-            _sptrAppWindow.RequestSize(new Size(500, 700));
+           // _sptrAppView.RequestSize(new Size(500, 700));
         }
         private void SptrAppWindowClosed(AppWindow sender, AppWindowClosedEventArgs args)
         {
-            _sptrAppWindow.Closed -= SptrAppWindowClosed;
-            _sptrAppWindow = null;
+          //  _sptrAppView.Closed -= SptrAppWindowClosed;
+            _sptrAppView = null;
             _rootGrid.Children.Remove(SplitExpenseUCtrl);
             SplitExpenseUCtrl.CanDrag = true;
             ThemeHelperService.UnRegisterElement(_rootGrid);
