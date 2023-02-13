2108e488a14e0af64ec5cc1877c436126cbf033e
SANJEI PRanav <sanjeipranav.me19@bitsathy.ac.in>
2023-02-13 10:03:30 +0530

    Removed INotifyFrom MainPage

--- SPLITTR_Uwp/Views/MainPage.xaml.cs
+++ SPLITTR_Uwp/Views/MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace SPLITTR_Uwp.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page, IMainView, INotifyPropertyChanged
+    public sealed partial class MainPage : Page, IMainView
     {
         IMainPageViewModel _viewModel;
         private static MainPage _view;
@@ -211,24 +211,7 @@ namespace SPLITTR_Uwp.Views
 
         #endregion
 
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        private bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value))
-                return false;
-            field = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-
-
-        #endregion
+       
 
 
     }
