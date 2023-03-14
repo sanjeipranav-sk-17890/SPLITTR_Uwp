c81d147b0f67c2bbd5f8581da57182573cdce568
SANJEI PRanav <sanjeipranav.me19@bitsathy.ac.in>
2023-03-14 11:39:53 +0530

    Edit Expense User access Ui changed

---
+++ SPLITTR_Uwp/Converters/BoolValueInversionConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SPLITTR_Uwp.Converters
+{
+    internal class BoolValueInversionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
